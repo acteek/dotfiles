890d979facc55a1c064fe9ca3c64e9c113ee3886
Sergey Ryazanov <sergey.ryazanov@ringcentral.com>
2025-02-07 17:57:16 +0100

    test

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,39 +1,38 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      filesystem = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        use_libuv_file_watcher = true,
-        filtered_items = {
-          visible = true,
-        },
-      },
-      default_component_config = {
-        git_status = {
-          -- Change type
-          added = "✚",
-          deleted = "✖",
-          modified = "",
-          renamed = "󰁕",
-          -- Status type
-          untracked = "",
-          ignored = "",
-          unstaged = "", --"󰄱"
-          staged = "",
-          conflict = "",
-        },
-        align = "right",
-      },
-    })
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				use_libuv_file_watcher = true,
+				filtered_items = {
+					visible = true,
+				},
+			},
+			git_status = {
+				-- Change type
+				added = "✚",
+				deleted = "✖",
+				modified = "",
+				renamed = "󰁕",
+				-- Status type
+				untracked = "",
+				ignored = "",
+				unstaged = "", --"󰄱"
+				staged = "",
+				conflict = "",
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		local events = require("neo-tree.events")
+		events.fire_event(events.GIT_EVENT)
+	end,
 }
