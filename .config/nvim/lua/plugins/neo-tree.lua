64944bef32def020393dee13b25fa684e0672fa6
Sergey Ryazanov <sergey.ryazanov@ringcentral.com>
2025-02-07 17:30:41 +0100

    test

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,21 +1,22 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      filesystem = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-    })
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-    local events = require("neo-tree.events")
-    events.fire_event(events.GIT_EVENT)
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				use_libuv_file_watcher = true,
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		local events = require("neo-tree.events")
+		events.fire_event(events.GIT_EVENT)
+	end,
 }
