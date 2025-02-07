b9bcad3f5ed0b4f945d1e8c9ec489ba6c8958fc7
Sergey Ryazanov <sergey.ryazanov@ringcentral.com>
2025-02-07 17:21:35 +0100

    update neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,19 +1,21 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			filesystem = {
-				hide_dotfiles = false,
-				hide_gitignored = false,
-			},
-		})
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      filesystem = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+    local events = require("neo-tree.events")
+    events.fire_event(events.GIT_EVENT)
+  end,
 }
