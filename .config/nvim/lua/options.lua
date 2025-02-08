ce0d21c7dbb93593871767314734f01b137e6093
Sergey Ryazanov <sergey.ryazanov@ringcentral.com>
2025-02-08 16:32:49 +0100

    hotkeys move lines

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -24,3 +24,6 @@ vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
+
+vim.keymap.set("n", "<C-j>", ":m +1<CR>", {})
+vim.keymap.set("n", "<C-k>", ":m -2<CR>", {})
